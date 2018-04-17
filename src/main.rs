302485434715f312e3e43172ce9c39c124090de0
adorechic <adorechic@gmail.com>
2018-04-18 00:40:50 +0900

    Add rusoto

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+extern crate rusoto_core;
+extern crate rusoto_dynamodb;
+
+use rusoto_core::Region;
+use rusoto_dynamodb::DynamoDbClient;
+
 fn main() {
+    let client = DynamoDbClient::simple(Region::ApNortheast1);
     println!("Hello, world!");
 }
